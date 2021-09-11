1469eff5ebc2750915d7d5fafb7b2345a2df18c5
shuimengzhi <shuimengzhi@gmail.com>
2021-09-11 23:42:28 +0800

    class内部添加属性

--- src/ClassT.php
+++ src/ClassT.php
@@ -12,23 +12,40 @@ class ClassT
     protected $classComment;
     protected $classCommentResult;
     protected $class;
+    protected $property;
 
     public function __construct($className)
     {
         $this->class = 'class ' . $className;
     }
 
-    public function classComment($comment)
+    public function addClassComment($comment)
     {
         $this->classComment .= ' * ' . $comment . PHP_EOL;
+    }
+
+    public function addPublicProperty($name)
+    {
+        $this->property .= PHP_EOL . '    public $' . $name . ';' . PHP_EOL;
+    }
+
+    public function addProtectProperty($name)
+    {
+        $this->property .= PHP_EOL . '    protected $' . $name . ';' . PHP_EOL;
+    }
+
+    public function addPrivateProperty($name)
+    {
+        $this->property .= PHP_EOL . '    private $' . $name . ';' . PHP_EOL;
     }
 
     public function __toString()
     {
         $this->classCommentResult = '/**' . PHP_EOL . $this->classComment . ' */' . PHP_EOL;
-        $this->string             .= $this->classCommentResult;
+        $this->string             = $this->classCommentResult . $this->class . PHP_EOL . "{" . PHP_EOL;
+        $this->string             .= $this->property;
 
-        return $this->string . $this->class . PHP_EOL . "{" . PHP_EOL . "}";
+        return $this->string . PHP_EOL . "}";
     }
 
 }
--- test/ClassTTest.php
+++ test/ClassTTest.php
@@ -19,8 +19,18 @@ class ClassTTest extends \PHPUnit\Framework\TestCase
     public function testClassComment()
     {
         $class = new ClassT('Test');
-        $class->classComment('@property string $goodsName');
-        $class->classComment('@property string $goodsName');
+        $class->addClassComment('@property string $goodsName');
+        $class->addClassComment('@property string $goodsName');
+        echo $class;
+    }
+
+    public function testProperty()
+    {
+        $class = new ClassT('Test');
+        $class->addPublicProperty('goods');
+        $class->addProtectProperty('pp');
+        $class->addClassComment('@property string $goodsName');
+        $class->addClassComment('@property string $goodsName');
         echo $class;
     }
 }
