94d73ecef25bf72c0f7bc3a37a793f9fb89484eb
shuimengzhi <shuimengzhi@gmail.com>
2021-09-11 23:23:55 +0800

    class 添加注释

--- src/ClassT.php
+++ src/ClassT.php
@@ -9,14 +9,23 @@ namespace src;
 class ClassT
 {
     protected $string;
+    protected $classComment;
+    protected $class;
 
     public function __construct($className)
     {
-        $this->string = 'class ' . $className;
+        $this->class = 'class ' . $className;
     }
 
     public function __toString()
     {
-        return $this->string . PHP_EOL . "{" . PHP_EOL . "}";
+        $this->string .= $this->classComment;
+
+        return $this->string . $this->class . PHP_EOL . "{" . PHP_EOL . "}";
+    }
+
+    public function addClassComment($classComment)
+    {
+        $this->classComment = $classComment;
     }
 }
--- test/ClassTTest.php
+++ test/ClassTTest.php
@@ -6,6 +6,7 @@ declare(strict_types=1);
 namespace test;
 
 
+use src\ClassCommentT;
 use src\ClassT;
 
 class ClassTTest extends \PHPUnit\Framework\TestCase
@@ -13,6 +14,16 @@ class ClassTTest extends \PHPUnit\Framework\TestCase
     public function testClass()
     {
         $class = new ClassT('Test');
+        echo $class;
+    }
+
+    public function testClassComment()
+    {
+        $class         = new ClassT('Test');
+        $classCommentT = new ClassCommentT();
+        $classCommentT->classComment('@property string $goodsName');
+        $classCommentT->classComment('@property string $goodsName');
+        $class->addClassComment($classCommentT);
         echo $class;
     }
 }
